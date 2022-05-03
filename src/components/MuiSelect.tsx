69a79e15615917977ef62a65e993efd0ec510721
jewoo <jeus0630@gmail.com>
2022-05-03 12:27:44 +0900

    Select박스 Mock Data를 기반으로 렌더링

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,8 +1,23 @@
 import {Box, TextField, MenuItem} from '@mui/material';
 import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+
+const fetchSelectList = async () => {
+    try{
+        const res = await fetch('http://localhost:3004/list');
+        const data = await res.json();
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+type MenuType = {
+    name: string
+}
 
 export default function MuiSelect(){
-
+    const {data, isLoading, isError} = useQuery('get-select-list',fetchSelectList);
     const [country, setCountry] = useState<string[]>([]);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,9 +32,11 @@ export default function MuiSelect(){
             <TextField label='Select country' select value={country} onChange={handleChange} fullWidth SelectProps={{
                 multiple: true
             }} size='small' color='secondary' helperText={!country.length? 'Please select your country': null} error={!country.length}>
-                <MenuItem value='IN'>India</MenuItem>
-                <MenuItem value='US'>USA</MenuItem>
-                <MenuItem value='AU'>Austrailia</MenuItem>
+                {
+                    data?.map((menu: MenuType) => (
+                        <MenuItem key={menu.name} value={menu.name}>{menu.name}</MenuItem>
+                    ))
+                }
             </TextField>
         </Box>
     )
