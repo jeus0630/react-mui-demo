a6a1d07ae9010cb144b4790fdc4a08777390f040
jewoo <jeus0630@gmail.com>
2022-05-04 13:25:46 +0900

    MUI Grid

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,7 +1,8 @@
-import { Box, Stack, Divider } from "@mui/material";
+import { Box, Stack, Divider, Grid } from "@mui/material";
 
 export default function MuiLayout(){
     return (        
+        <>
         <Stack sx={{
             border: '1px solid'
         }}
@@ -28,8 +29,22 @@ export default function MuiLayout(){
             bgcolor='success.light'
             p={2}
             >
-
             </Box>
         </Stack>
+        <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+            <Grid item xs='auto' sm={6}>
+                <Box bgcolor={'primary.light'} p={2}>Item 1</Box>
+            </Grid>
+            <Grid item xs='auto' sm={6}>
+                <Box bgcolor={'primary.light'} p={2}>Item 2</Box>
+            </Grid>
+            <Grid item xs='auto' sm={6}>
+                <Box bgcolor={'primary.light'} p={2}>Item 3</Box>
+            </Grid>
+            <Grid item xs='auto' sm={6}>
+                <Box bgcolor={'primary.light'} p={2}>Item 4</Box>
+            </Grid>
+        </Grid>
+        </>
     )
 }
