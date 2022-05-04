9950195d4132f587d05381de794db3cbc2579136
jewoo <jeus0630@gmail.com>
2022-05-04 14:47:42 +0900

    MUI Paper

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,50 +1,50 @@
-import { Box, Stack, Divider, Grid } from "@mui/material";
+import { Box, Stack, Divider, Grid, Paper } from "@mui/material";
 
 export default function MuiLayout(){
     return (        
-        <>
-        <Stack sx={{
-            border: '1px solid'
-        }}
-        direction='row'
-        spacing={2}
-        divider={<Divider orientation="vertical" flexItem/>}
-        >
-            <Box sx={{
-                backgroundColor:'primary.main',
-                color:'white',
-                width:'100px',
-                height:'100px',
-                padding:'16px',
-                '&:hover': {
-                    backgroundColor: 'primary.light'
-                }
-            }}>
-                Codevolution
-            </Box>
-            <Box 
-            display={'flex'} 
-            height='100px' 
-            width='100px' 
-            bgcolor='success.light'
-            p={2}
+        <Paper sx={{ padding : '32px'}} elevation={4}>
+            <Stack sx={{
+                border: '1px solid'
+            }}
+            direction='row'
+            spacing={2}
+            divider={<Divider orientation="vertical" flexItem/>}
             >
-            </Box>
-        </Stack>
-        <Grid container my={4} rowSpacing={2} columnSpacing={1}>
-            <Grid item xs='auto' sm={6}>
-                <Box bgcolor={'primary.light'} p={2}>Item 1</Box>
+                <Box sx={{
+                    backgroundColor:'primary.main',
+                    color:'white',
+                    width:'100px',
+                    height:'100px',
+                    padding:'16px',
+                    '&:hover': {
+                        backgroundColor: 'primary.light'
+                    }
+                }}>
+                    Codevolution
+                </Box>
+                <Box 
+                display={'flex'} 
+                height='100px' 
+                width='100px' 
+                bgcolor='success.light'
+                p={2}
+                >
+                </Box>
+            </Stack>
+            <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+                <Grid item xs='auto' sm={6}>
+                    <Box bgcolor={'primary.light'} p={2}>Item 1</Box>
+                </Grid>
+                <Grid item xs='auto' sm={6}>
+                    <Box bgcolor={'primary.light'} p={2}>Item 2</Box>
+                </Grid>
+                <Grid item xs='auto' sm={6}>
+                    <Box bgcolor={'primary.light'} p={2}>Item 3</Box>
+                </Grid>
+                <Grid item xs='auto' sm={6}>
+                    <Box bgcolor={'primary.light'} p={2}>Item 4</Box>
+                </Grid>
             </Grid>
-            <Grid item xs='auto' sm={6}>
-                <Box bgcolor={'primary.light'} p={2}>Item 2</Box>
-            </Grid>
-            <Grid item xs='auto' sm={6}>
-                <Box bgcolor={'primary.light'} p={2}>Item 3</Box>
-            </Grid>
-            <Grid item xs='auto' sm={6}>
-                <Box bgcolor={'primary.light'} p={2}>Item 4</Box>
-            </Grid>
-        </Grid>
-        </>
+        </Paper>
     )
 }
